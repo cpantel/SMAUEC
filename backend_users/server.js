7c8a9bd6e8b382f7ed73b9b6e0090ee00200a169
Carlos Pantelides <carlos.pantelides@gmail.com>
2020-07-18 19:00:28 -0300

    preparing second backend

--- backend_users/server.js
+++ backend_users/server.js
@@ -37,7 +37,7 @@ require('./app/routes/auth.routes')(app);
 require('./app/routes/user.routes')(app);
 
 // set port, listen for requests
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 8081;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
