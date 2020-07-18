587a6a29a76b4bd8f2f98b451f062ad5a16d9e42
Carlos Pantelides <carlos.pantelides@gmail.com>
2020-07-18 17:05:53 -0300

    initial commit

---
+++ backend/app/config/db.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  HOST: "172.22.0.1",
+  USER: "postgres",
+  PASSWORD: "12345602",
+  DB: "smauec",
+  dialect: "postgres"
+};
---
+++ backend/app/models/user.model.js
@@ -0,0 +1,12 @@
+module.exports = (sequelize, Sequelize) => {
+  const User = sequelize.define("users", {
+    username: {
+      type: Sequelize.STRING
+    },
+    password: {
+      type: Sequelize.STRING
+    }
+  });
+
+  return User;
+};
