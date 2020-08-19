2f1e709574cf189756cbcd6a3662891f09068cf3
Carlos Pantelides <carlos.pantelides@gmail.com>
2020-08-19 15:19:37 -0300

    partially exposed roles to API

---
+++ api_users/app/controllers/role.controller.js
@@ -0,0 +1,99 @@
+const db = require("../models");
+const Op = db.Sequelize.Op;
+const Role = db.role;
+
+var findTheOne = (id, res, status) => {
+  Role.findByPk(
+    id, 
+    { attributes: {
+        exclude: ['createdAt', 'updatedAt']
+      }
+    }
+  ).then(role => {
+    if (null == role) {
+      return res.status(403).send({
+        status: 403,
+        message: "Role not found",
+        result: {}
+      });
+    } else {
+      return res.status(status).send(
+        { 
+          status: status,
+          message: "role ok",
+          result: role
+        }
+      )
+    }
+  }).catch(err => {
+    return res.status(500).send({
+          status: 500,
+          message: "Role not found",
+          result: {}
+    });
+  }
+)};
+
+exports.findOne = (req, res) => {
+  return findTheOne(req.params.roleId, res, 200);
+};
+
+
+exports.findAll = (req, res) => {
+  Role.findAll({
+    attributes: ['id', 'name']
+  }).then(roles => {
+    res.status(200).send({
+      status:200,
+      message: "roles list",
+      result: roles
+    });
+  })}
+
+exports.delete = (req, res) => {
+  Role.destroy(
+    {where: {id: req.params.roleId}}
+  ).then( num=> {
+     if (1 == num) {
+        res.status(200).send({
+          status: 200,
+          message: "Role deleted",
+          result: {}
+        })
+     } else { 
+      res.status(404).send({
+          status: 404,
+          message: "Role not found",
+          result: {}
+      })
+     }}
+  );
+}
+
+exports.create = (req, res) => {
+  // Save User to Database
+  Role.create({
+    name: req.body.username
+  })
+    .then(role => {
+      res.status(201).send({ 
+        status: 201,
+        message: "Role created",
+        result: role
+      });
+    })
+    .catch(err => {
+      res.status(500).send({ 
+        status: 500,
+        message: err.message,
+        result: {}
+      });
+    });
+};
+
+
+// Update a User by the id in the request
+exports.update = (req, res) => {
+  
+};
+
--- api_users/server.js
+++ api_users/server.js
@@ -51,6 +51,7 @@ app.get("/", (req, res) => {
 // routes
 require('./app/routes/auth.routes')(app);
 require('./app/routes/user.routes')(app);
+require('./app/routes/role.routes')(app);
 
 // set port, listen for requests
 const PORT = process.env.PORT || 8081;
